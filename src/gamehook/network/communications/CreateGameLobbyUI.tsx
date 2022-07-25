733594dd0e81de312ec2e54d7aad71ef4337d905
Robert Townley <me@roberttownley.com>
2022-07-24 22:27:38 -0400

    Restarting multiplayer

--- src/gamehook/network/communications/CreateGameLobbyUI.tsx
+++
@@ -1,7 +0,0 @@
-import { ConnectionUIProps } from "./types";
-
-interface Props extends ConnectionUIProps {}
-
-export function CreateGameLobbyUI({}: Props) {
-  return <h1>Create Game Lobby</h1>;
-}
--- src/gamehook/network/communications/types.tsx
+++
@@ -1,7 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-
-import { ConnectionStatus } from "../types";
-
-export interface ConnectionUIProps {
-  setStatus: Dispatch<SetStateAction<ConnectionStatus>>;
-}
